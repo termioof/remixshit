cb7bdb6d9bb402abd41b8e87b7245f21caecf550
termioof <94211501+termioof@users.noreply.github.com>
2021-11-13 05:32:27 +0100

    Create Context.sol

---
+++ Context.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: Unlicensed
+
+pragma solidity ^0.8.7;
+
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+}
